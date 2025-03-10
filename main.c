299fc824c0368d77f8af675087a3a976f524c7bf
CookingCanadian <cookedcanadian@gmail.com>
2025-03-10 19:42:41 -0400

    more font stuff

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-#include "raylib.h"
 #include <stdio.h>
+#include "raylib.h"
+#include "GFSNeohellenic_Italic.h"
+#include "GFSNeohellenic_Bold.h"
+#include "GFSNeohellenic_BoldItalic.h"
 
-// Helper function to convert hex colour (e.g., "#FFFFFF") to raylib color
+// Function to convert hex color (e.g., "#FFFFFF") to raylib Color
 Color toHex(const char* hex) {
     if (hex[0] == '#') hex++;
     
@@ -10,12 +13,37 @@ Color toHex(const char* hex) {
     return (Color){ (unsigned char)r, (unsigned char)g, (unsigned char)b, 255 }; 
 }
 
+// Global font variables
+Font italicGFS;
+Font boldGFS_h1;
+Font boldGFS_h2;
+Font boldItalicGFS;
+
+// Function to load fonts
+void loadFonts() {
+    italicGFS = LoadFontFromMemory(".ttf", font_data_italic, sizeof(font_data_italic), 12, NULL, 0);
+    boldGFS_h1 = LoadFontFromMemory(".ttf", font_data_bold, sizeof(font_data_bold), 20, NULL, 0);
+    boldGFS_h2 = LoadFontFromMemory(".ttf", font_data_bold, sizeof(font_data_bold), 14, NULL, 0);
+    boldItalicGFS = LoadFontFromMemory(".ttf", font_data_bold_italic, sizeof(font_data_bold_italic), 12, NULL, 0);
+}
+
+// Function to unload fonts
+void unloadFonts() {
+    UnloadFont(italicGFS);
+    UnloadFont(boldGFS_h1);
+    UnloadFont(boldGFS_h2);
+    UnloadFont(boldItalicGFS);
+}
+
 int main(void) {
     const int screenWidth = 720;
     const int screenHeight = 360;
 
     InitWindow(screenWidth, screenHeight, "noctivox | a virtual piano player");
     SetTargetFPS(60);
+
+    // Load fonts 
+    loadFonts();
 
     // Static background buffer
     RenderTexture2D backgroundTexture = LoadRenderTexture(screenWidth, screenHeight);
@@ -32,21 +60,26 @@ int main(void) {
         DrawRectangle(14, 48, 180, 1, toHex("#272C3C")); 
         DrawRectangle(222, 85, 210, 1, toHex("#494D5A")); 
         DrawRectangle(355, 312, 1, 36, toHex("#494D5A")); 
+
+        // Unchanging Text
+        DrawTextEx(boldGFS_h1, "noctivox", (Vector2){ 14, 8 }, 20, 1, toHex("#FFFFFF"));
+        DrawTextEx(boldGFS_h1, "tempo:", (Vector2){ 226, 318 }, 20, 1, toHex("#9CA2B7"));
+        DrawTextEx(italicGFS, "a virtual piano player", (Vector2){ 14, 30 }, 12, 1, toHex("#FFFFFF"));
     EndTextureMode();
 
     while (!WindowShouldClose()) {
+        Vector2 mousePosition = GetMousePosition();
+
         BeginDrawing();
-            DrawTextureRec(backgroundTexture.texture, 
-                          (Rectangle){ 0, 0, screenWidth, -screenHeight }, 
-                          (Vector2){ 0, 0 }, 
-                          WHITE);
+            DrawTextureRec(backgroundTexture.texture, (Rectangle){ 0, 0, screenWidth, -screenHeight }, (Vector2){ 0, 0 }, WHITE);
 
-
+        
         EndDrawing();
     }
 
     // Cleanup
     UnloadRenderTexture(backgroundTexture);
+    unloadFonts(); 
     CloseWindow();
     return 0;
 }
